2cd48bee4f9ee91819ddf680812ceedfbd91d422
Anamitra Datta <anamitradatta@gmail.com>
2019-10-30 18:34:36 -0400

    added loop added photo directory

--- mqtt_publish_camera.py
+++ mqtt_publish_camera.py
@@ -1,6 +1,3 @@
-# MQTT Publish Demo
-# Publish two messages, to two different topics
-
 import paho.mqtt.publish as publish
 from picamera import PiCamera
 from time import sleep
@@ -17,31 +14,34 @@ camera = PiCamera()
 pir = MotionSensor(4)
 #message = "Hello"
 
-sleep(10)
-print("Done sleeping")
-message = "Motion Not Detected"
-#index = 0
-
 while True:
-	sleep(1)
-	print(pir.motion_detected)
-	if pir.motion_detected == True:
-		print("If Statement True")
-		message = "Motion Detected"
-		break;
-	#index+=1
+	print("Sensor resting")
+	sleep(10)
+	print("Done sleeping")
+	message = "Motion Not Detected"
+	#index = 0
+
+	while True:
+		sleep(1)
+		print(pir.motion_detected)
+		if pir.motion_detected == True:
+			print("If Statement True")
+			message = "Motion Detected"
+			break;
+		#index+=1
 
 
-print("Motion has been detected")
+	print("Motion has been detected")
 
-publish.single(MQTT_PATH, message, hostname=MQTT_SERVER)
-print("Sent message: ", message)
-camera.resolution=(2592,1944)
-#camera.framerate=15
-camera.start_preview()
-#sleep(5)
-date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S_%f")
-camera.capture('/home/pi/Desktop/' + date + '.jpg')
-camera.capture('/home/pi/Desktop/test.jpg')
-camera.stop_preview()
-print(datetime.datetime.now())
+	publish.single(MQTT_PATH, message, hostname=MQTT_SERVER)
+	print("Sent message: ", message)
+	camera.resolution=(2592,1944)
+	#camera.framerate=15
+	camera.start_preview()
+	#sleep(5)
+	date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S_%f")
+	#camera.capture('/home/pi/Desktop/' + date + '.jpg')
+	camera.capture('/home/pi/Desktop/cameraTrapPhotos/' + date + '.jpg')
+	#camera.capture('/home/pi/Desktop/test.jpg')
+	camera.stop_preview()
+	print(datetime.datetime.now())
