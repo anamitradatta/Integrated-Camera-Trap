62cc45b24416a3e08481e15bb47a18910f5525df
Anamitra Datta <anamitradatta@gmail.com>
2020-04-19 16:20:29 -0400

    Update timesyncssh.sh

--- AP_Files/timesyncssh.sh
+++ AP_Files/timesyncssh.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+#Get the list of all IP addresses in the current network
 nmap -sn -oG ip.txt 192.168.5.1-255 > /dev/null
+
+#get the host machines's IP address
 iphost=$(hostname -I)
 #echo "$iphost"
+
+#make a list of all IP addresses besides the host's IP address
 iplist=($(cat ip.txt | grep Host: | awk -v h=$iphost 'NR> 1 {if ($2!=h) print $2}'))
 #printf '%s\n' "${iplist[@]}"
-#ssh-keygen -b 2048 -t rsa -f /home/pi/.ssh/id_rsa -q -N "" 0>&-
+
+#Make an SSH key for the network 
+ssh-keygen -b 2048 -t rsa -f /home/pi/.ssh/id_rsa -q -N "" 0>&-
+
+#Copy the SSH key into every Pi. NEEDS TO BE IMPROVED
 #for i in "${iplist[@]}"
 #do
 	#sudo sshpass -p "raspberry" ssh-copy-id -i ~/.ssh/id_rsa pi@$i
 	#ssh-copy-id -i ~/.ssh/id_rsa pi@$i
 #done
+
+#Set the time in each Raspberry Pi from the RTC through ssh
 for i in "${iplist[@]}"
 do
 	#echo $i
