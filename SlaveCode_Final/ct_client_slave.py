7df33d850bb1e63c2648c345e5b725cb80ebc38a
Anamitra Datta <anamitradatta@gmail.com>
2020-04-19 22:45:29 -0400

    Update ct_client_slave.py

--- SlaveCode_Final/ct_client_slave.py
+++ SlaveCode_Final/ct_client_slave.py
@@ -18,12 +18,13 @@ MQTT_SERVER = "192.168.5.101" #IP address of main pi
 MQTT_PATH = "test" #mqtt topic for CT program
 path = '/home/pi/cameraTrapPhotos/' #directory for photos
 camera = PiCamera() #use the camera
+cameraNum = 2 #camera number, needs to be different for each pi
 
 #method to subscribe to MQTT topic
 def on_connect(client, userdata, flags, rc):
     #result code 0 = successful
     print("Connected with result code "+str(rc))
-    print("Started the photo synchronization program on slave 2. Waiting for a signal from the master module")
+    print("Started the photo synchronization program on slave "+ str(cameraNum) + " Waiting for a signal from the master module")
     # Subscribing in on_connect() - if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(MQTT_PATH)
@@ -47,16 +48,16 @@ def on_message(client, userdata, msg):
             print("sucessfully created the directory %s" % path)
             
         #set up camera and file parameters
-        photoName = 'set' + str(photoNum) + '_camera2.jpg'
+        photoName = 'set' + str(photoNum) + '_camera' + str(cameraNum) + '.jpg'
         camera.resolution = (3240,2464)
         camera.shutter_speed = 30000
         
         #take photo 
         camera.capture(path + photoName)
-        print("Photo Number", photoNum, " taken on Slave 2")
+        print("Photo Number", photoNum, " taken on Slave " + str(cameraNum))
         
         #FTP to master pi 
-        print("Moving from Cam2 to Master")
+        print("Moving from Cam " + str(cameraNum) +" to Master")
         ftp = FTP(MQTT_SERVER)
         ftp.login('pi','raspberry')
         pathMaster = '/home/pi/cameraTrapPhotos/set' + str(photoNum) + '/'
@@ -66,7 +67,7 @@ def on_message(client, userdata, msg):
         except OSError:
             print("could not open file")
         ftp.storbinary('STOR ' + pathMaster + photoName, file)
-        print("finished moving photo from Cam2 to master")
+        print("finished moving photo from Cam " + str(cameraNum)+ " to master")
         
         file.close()
         ftp.quit()
